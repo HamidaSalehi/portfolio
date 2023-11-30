54da83cf81be8d9f69a5235d95d07301b021c3f9
Hamida Salehi <hamidasalehi84@gmail.com>
2023-11-30 10:04:27 +0430

    links were given to each project

--- src/component/Works/works.js
+++ src/component/Works/works.js
@@ -9,13 +9,25 @@ const Works = () => {
     <section id="Works">
       <h2 className="worrksTitle">My protfolio </h2>
       <div className="worksImgs">
-        <a href="./DiceGame/index.html" target="_blank" rel="noreferrer">
+        <a
+          href="https://hamidasalehi.github.io/Dice-game/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={protfolio1} alt="" className="worksImg" />
         </a>
-        <a href="./NumberGuess/index.html" target="_blank" rel="noreferrer">
+        <a
+          href="https://hamidasalehi.github.io/number-guess/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={protfolio2} alt="" className="worksImg" />
         </a>
-        <a href="./showmodal/index.html" target="_blank" rel="noreferrer">
+        <a
+          href="https://hamidasalehi.github.io/show-modal/"
+          target="_blank"
+          rel="noreferrer"
+        >
           <img src={protfolio3} alt="" className="worksImg" />
         </a>
       </div>
